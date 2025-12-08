64dc4febc000c15712da11e5ee6d257e2d979988
Masaya <masaya31507214@gmail.com>
2025-12-08 13:11:51 +0900

    機能修正

--- src/main/java/com/example/kakeibo/model/IncomeExpense.java
+++ src/main/java/com/example/kakeibo/model/IncomeExpense.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Column;
 
 @Entity
 public class IncomeExpense {
@@ -12,28 +13,49 @@ public class IncomeExpense {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String monthName; // 月
     private int income;
     private int expense;
 
+    @Column(name = "month_name") // H2で予約語回避
+    private int month;
+
     public IncomeExpense() {}
 
-    public IncomeExpense(String monthName, int income, int expense) {
-        this.monthName = monthName;
+    public IncomeExpense(int income, int expense, int month) {
         this.income = income;
+        this.expense = expense;
+        this.month = month;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getIncome() {
+        return income;
+    }
+
+    public void setIncome(int income) {
+        this.income = income;
+    }
+
+    public int getExpense() {
+        return expense;
+    }
+
+    public void setExpense(int expense) {
         this.expense = expense;
     }
 
-    // Getter & Setter
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public int getMonth() {
+        return month;
+    }
 
-    public String getMonthName() { return monthName; }
-    public void setMonthName(String monthName) { this.monthName = monthName; }
-
-    public int getIncome() { return income; }
-    public void setIncome(int income) { this.income = income; }
-
-    public int getExpense() { return expense; }
-    public void setExpense(int expense) { this.expense = expense; }
+    public void setMonth(int month) {
+        this.month = month;
+    }
 }
